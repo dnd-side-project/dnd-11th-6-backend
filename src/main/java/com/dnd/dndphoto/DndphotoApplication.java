b2fc6d7100d65fe34a0b02a0f4403969c599f43d
LeeJaeHoon <dlwogns3413@naver.com>
2024-07-11 21:37:47 +0900

    first commit

---
+++ src/main/java/com/dnd/dndphoto/DndphotoApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.dndphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndphotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndphotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/dndphoto/DndphotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.dndphoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndphotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
