dcf63dfa875d3a9b9cc060086e410228660afc20
chaewon-io <0101angela@naver.com>
2024-07-29 17:01:12 +0900

    Chore: Spring REST Docs 환경 세팅

---
+++ src/test/java/com/dnd/dndphoto/config/RestDocsConfiguration.java
@@ -0,0 +1,25 @@
+package com.dnd.dndphoto.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import static org.springframework.restdocs.snippet.Attributes.Attribute;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler write() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+        );
+    }
+
+    public static Attribute field(final String key, final String value) {
+        return new Attribute(key, value);
+    }
+}
