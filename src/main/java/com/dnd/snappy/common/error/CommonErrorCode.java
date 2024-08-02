8431a89088833dcdf7fa2cb8b090b322ce3bb280
LeeJaeHoon <dlwogns3413@naver.com>
2024-08-02 21:05:58 +0900

    feat: GlobalExceptionHandler 추가 (#25)

--- src/main/java/com/dnd/snappy/common/error/CommonErrorCode.java
+++ src/main/java/com/dnd/snappy/common/error/CommonErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCodeInterface {
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON-001", "요청을 이해할 수 없거나 필수 매개변수가 누락되었습니다."),;
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON-001", "요청을 이해할 수 없거나 필수 매개변수가 누락되었습니다."),
+    ;
 
     private final HttpStatus status;
 
