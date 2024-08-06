6716000536250b7ef5709f3822ad6d0f836c917c
LeeJaeHoon <dlwogns3413@naver.com>
2024-08-07 01:21:46 +0900

    feat: refreshToken redis entity 구성 (#20)

---
+++ src/main/java/com/dnd/snappy/domain/token/entity/RefreshToken.java
@@ -0,0 +1,37 @@
+package com.dnd.snappy.domain.token.entity;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "refreshToken")
+public class RefreshToken {
+    @Id
+    private Long memberId;
+
+    private String token;
+
+    @TimeToLive
+    private Long ttl;
+
+    @Builder
+    private RefreshToken(Long memberId, String token, Long ttl) {
+        this.memberId = memberId;
+        this.token = token;
+        this.ttl = ttl;
+    }
+
+    public static RefreshToken create(Long memberId, String token, Long ttl) {
+        return RefreshToken.builder()
+                .memberId(memberId)
+                .token(token)
+                .ttl(ttl)
+                .build();
+    }
+}
