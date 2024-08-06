01a2834bd188ab62c00999f80b06a8f379a611b2
LeeJaeHoon <dlwogns3413@naver.com>
2024-08-07 01:33:06 +0900

    feat: 토큰 생성시 refresh token redis에 저장 로직 구현 (#20)

--- src/main/java/com/dnd/snappy/domain/token/entity/RefreshToken.java
+++ src/main/java/com/dnd/snappy/domain/token/entity/RefreshToken.java
@@ -1,10 +1,10 @@
 package com.dnd.snappy.domain.token.entity;
 
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
--- src/main/java/com/dnd/snappy/domain/token/service/TokenService.java
+++ src/main/java/com/dnd/snappy/domain/token/service/TokenService.java
@@ -1,6 +1,8 @@
 package com.dnd.snappy.domain.token.service;
 
 import com.dnd.snappy.domain.token.dto.Tokens;
+import com.dnd.snappy.domain.token.entity.RefreshToken;
+import com.dnd.snappy.domain.token.repository.RefreshTokenRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +10,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TokenService {
 
+    private final static Long REFRESH_TOKEN_TTL = 3600L; //1시간
+
     private final TokenProvider tokenProvider;
+
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
 
     public Tokens createTokens(Long memberId) {
         Tokens tokens = tokenProvider.issueTokens(memberId);
-        //TODO: refreshToken redis에 저장
-
+        refreshTokenRedisRepository.save(RefreshToken.create(memberId, tokens.refreshToken(), REFRESH_TOKEN_TTL));
         return tokens;
     }
 }
