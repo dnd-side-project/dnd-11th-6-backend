a83e58d59d005bb456698d60296208d0932ad804
chaewon-io <0101angela@naver.com>
2024-08-24 00:18:01 +0900

    fix: LEFT JOIN 사용하도록 쿼리 수정 (#41)

--- src/main/java/com/dnd/snappy/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/dnd/snappy/domain/mission/repository/MissionRepository.java
@@ -5,6 +5,7 @@ import com.dnd.snappy.domain.mission.entity.Mission;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,11 +16,14 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     List<Mission> findAllByMeetingId(Long meetingId);
 
-    @Query("SELECT new com.dnd.snappy.domain.mission.dto.response.LeaderMeetingMissionDetailResponseDto(m.id, m.content, " +
-            "EXISTS (SELECT 1 FROM MissionParticipant mp WHERE mp.mission.id = m.id)) " +
-            "FROM Mission m " +
-            "JOIN Participant p ON p.meeting.id = m.meeting.id " +
-            "WHERE p.id = :participantId AND p.role = 'LEADER' AND m.meeting.id = :meetingId")
+    @Query("""
+    SELECT new com.dnd.snappy.domain.mission.dto.response.LeaderMeetingMissionDetailResponseDto(m.id, m.content, 
+        CASE WHEN mp.id IS NOT NULL THEN TRUE ELSE FALSE END) 
+    FROM Mission m 
+    LEFT JOIN MissionParticipant mp ON mp.mission.id = m.id 
+    JOIN Participant p ON p.meeting.id = m.meeting.id 
+    WHERE p.id = :participantId AND p.role = 'LEADER' AND m.meeting.id = :meetingId
+    """)
     List<LeaderMeetingMissionDetailResponseDto> findLeaderMeetingMissions(@Param("meetingId") Long meetingId, @Param("participantId") Long participantId);
 
 }
