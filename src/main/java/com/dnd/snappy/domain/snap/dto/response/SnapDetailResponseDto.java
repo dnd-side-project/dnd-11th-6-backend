0100502d67715ebdc676dba6afab963e553ac2be
LeeJaeHoon <dlwogns3413@naver.com>
2024-08-23 12:32:29 +0900

    test: snap 상세 정보 조회 api test (#63)

--- src/main/java/com/dnd/snappy/domain/snap/dto/response/SnapDetailResponseDto.java
+++ src/main/java/com/dnd/snappy/domain/snap/dto/response/SnapDetailResponseDto.java
@@ -1,11 +1,13 @@
 package com.dnd.snappy.domain.snap.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 
 public record SnapDetailResponseDto(
         Long snapId,
         String snapUrl,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
         LocalDateTime shootDate,
         String type,
         ParticipantDetailResponseDto photographer,
