0bbd78d5f12ff65754d3667555f11f1463c124cd
chaewon-io <0101angela@naver.com>
2024-09-15 18:49:19 +0900

    fix: 모임 링크 만료 예외 처리 로직 추가 (#79)

--- src/main/java/com/dnd/snappy/domain/meeting/service/MeetingService.java
+++ src/main/java/com/dnd/snappy/domain/meeting/service/MeetingService.java
@@ -11,6 +11,8 @@ import com.dnd.snappy.domain.meeting.dto.response.CreateMeetingResponseDto;
 import com.dnd.snappy.domain.meeting.dto.response.MeetingDetailResponseDto;
 import com.dnd.snappy.domain.meeting.dto.response.ShareMeetingLinkResponseDto;
 import com.dnd.snappy.domain.meeting.entity.Meeting;
+import com.dnd.snappy.domain.meeting.entity.MeetingLinkStatus;
+import com.dnd.snappy.domain.meeting.exception.MeetingErrorCode;
 import com.dnd.snappy.domain.meeting.repository.MeetingRepository;
 import com.dnd.snappy.infrastructure.uploader.ImageUploader;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +36,9 @@ public class MeetingService {
     @Transactional(readOnly = true)
     public MeetingDetailResponseDto findByMeetingLink(String meetingLink) {
         Meeting meeting = findByMeetingLinkOrThrow(meetingLink);
+
+        validateMeetingStatus(meeting);
+
         return new MeetingDetailResponseDto(meeting);
     }
 
@@ -80,6 +85,9 @@ public class MeetingService {
 
     public MeetingDetailResponseDto findMeetingDetailById(Long meetingId) {
         Meeting meeting = findByMeetingIdOrThrow(meetingId);
+
+        validateMeetingStatus(meeting);
+
         return new MeetingDetailResponseDto(meeting);
     }
 
@@ -91,6 +99,12 @@ public class MeetingService {
     private Meeting findByMeetingLinkOrThrow(String meetingLink) {
         return meetingRepository.findByMeetingLink(meetingLink)
                 .orElseThrow(() -> new NotFoundException(MEETING_LINK_NOT_FOUND, "[meetingLink: " + meetingLink + " is not found]"));
+    }
+
+    private void validateMeetingStatus(Meeting meeting) {
+        if (meeting.getMeetingLinkStatus() == MeetingLinkStatus.EXPIRED) {
+            throw new BusinessException(MeetingErrorCode.MEETING_LINK_EXPIRED);
+        }
     }
 
     private void checkMeetingLinkDuplication(String meetingLink) {
